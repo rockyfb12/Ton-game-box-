4a559cfb25d2109361cdec319131831d892db070
rockyfb12 <reportfb12@gmail.com>
2025-06-21 04:05:17 +0600

    Update HomeScreen.jsx

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import WalletConnect from "./WalletConnect";
+import Deposit from "./Deposit";
+import Withdraw from "./Withdraw";
+import BoxDrawGame from "./BoxDrawGame";
+import InviteEarn from "./InviteEarn";
+import ChannelButton from "./ChannelButton";
 
 export default function HomeScreen() {
   return (
-    <div>
-      <p>Welcome to the TON GameBox Home!</p>
+    <div className="p-4 space-y-4 text-center">
+      <h1 className="text-2xl font-bold">🎮 TON GameBox</h1>
+      <WalletConnect />
+      <Deposit />
+      <Withdraw />
+      <BoxDrawGame />
+      <InviteEarn />
+      <ChannelButton />
     </div>
   );
 }
